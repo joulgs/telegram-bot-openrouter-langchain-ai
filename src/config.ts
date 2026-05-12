cfd216e13b8fccf1608850a42127f27240ea5d2c
Jonathas Soares <joul.gs@gmail.com>
2026-05-12 20:41:09 -0300

    Update package dependencies and improve environment variable assertion

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-console.assert(process.env.TELEGRAM_BOT_TOKEN, '⚠️  TELEGRAM_BOT_TOKEN is not defined in environment variables')
+console.assert(!!process.env.TELEGRAM_BOT_TOKEN, '⚠️  TELEGRAM_BOT_TOKEN is not defined in environment variables')
 
 export const config = {
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '',
