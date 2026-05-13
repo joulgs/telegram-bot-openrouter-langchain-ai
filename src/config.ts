d4ca14f98923ad05b83bc00bd3ecc7a3ccb88c90
Jonathas Soares <joul.gs@gmail.com>
2026-05-12 22:07:28 -0300

    Add OPENROUTER_API_KEY to environment variables and update config structure

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,24 @@
 console.assert(!!process.env.TELEGRAM_BOT_TOKEN, '⚠️  TELEGRAM_BOT_TOKEN is not defined in environment variables')
+console.assert(!!process.env.OPENROUTER_API_KEY, '⚠️  OPENROUTER_API_KEY is not defined in environment variables')
 
 export const config = {
   telegram: { botToken: process.env.TELEGRAM_BOT_TOKEN || '' },
+  openRouter: {
+    apiKey: process.env.OPENROUTER_API_KEY,
+    httpRefer: 'https://github.com/joulgs/telegram-bot-openrouter-langchain-ai',
+    models: [
+      'google/gemma-4-31b-it:free',
+      'openrouter/owl-alpha',
+      'qwen/qwen3-coder:free',
+    ],
+    temperature: 0.2,
+    maxTokens: 50,
+    systemPrompt: 'You are a helpful assistant.',
+    provider: {
+      sort: {
+        by: 'price', // 'price' | 'latency' | 'throughput'
+        partition: 'none'
+      }
+    }
+  }
 };
